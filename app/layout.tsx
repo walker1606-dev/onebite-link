668a851258a7f4ac0499667fb4ef1a00298f6538
조호성 <walker33@naver.com>
2026-08-31 13:55:48 +0900

    변경사항 커밋 및 푸시

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,6 +45,20 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="ko"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "yatjtxmmna");
+            `,
+          }}
+        />
+      </head>
       <body className="flex h-full flex-col overflow-hidden">{children}</body>
     </html>
   );
